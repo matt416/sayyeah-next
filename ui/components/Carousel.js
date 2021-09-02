598ea3a802107c19b963df074637e4796eecb4b9
Matt Rintoul <matt@sayyeah.com>
2021-09-02 16:35:26 -0400

    Refactor carousel

--- ui/components/Carousel.js
+++ ui/components/Carousel.js
@@ -1,134 +1,134 @@
-import { useEffect, useRef, useState, Children } from "react"
+import { useEffect, useRef, useState, Children, Fragment } from "react"
 import styles from "./Carousel.module.css"
 // import clsx from 'clsx'
 import Arrow from "ui/svg/Arrow.svg"
-
 
 import smoothscroll from 'smoothscroll-polyfill';
 
 const nextClosest = (array, goal) =>
   array.find(item => item <= goal  ? false : true);
 
-const prevClosest = (array, goal) => array.slice().reverse().find(item => item >= goal  ? false : true);
+const prevClosest = (array, goal) => array.slice().reverse().find(item => item >= goal  ? false : true); // .slice ensures we don't mutate and reverse the original array
 
-const closest = (direction = 'next', array, goal) =>
-  direction === 'prev' ? prevClosest(array, goal) : nextClosest(array, goal)
+const closest = (direction = 'next', array, goal) => {
+  // Direction = next or prev
+  return direction === 'next' ? nextClosest(array, goal) : prevClosest(array, goal)
+}
 
+/*** Determine if navigation button is enabled or not */
+const useNavButtons = () => {
+  const [next, setNext] = useState(false)
+  const [prev, setPrev] = useState(false)
 
-export default function Carousel({ children, className, size }){
-  const $carousel = useRef(null)
-  const $items = useRef([]) // Array of refs for child nodes
-  // const [currentItem, setCurrentItem] = useState(0)
-  const style = size ? { "--card-size" : size } : {} // Modify css variables here
-  const [navigationInactive, setNavigationInactive] = useState({ next: false, prev: false })
+  const setNavButtons = (slides, target) => {
+    // if (!slides || !target) { return }
+    setNext(slides.indexOf(target) === slides.length - 1)
+    setPrev(slides.indexOf(target) <= 0)
+  }
 
+  return [{ next, prev }, setNavButtons]
+}
+
+const useSmoothScroll = () => {
   useEffect(() => {
     // Fix for smooth scrolling in Safari.
     smoothscroll.polyfill();
-
-    // const target = closest('prev', items, currentScrollPosition)
-
-    // console.log(target, items)
-    // setCurrentItem(items.indexOf(target))
-    const currentScrollPosition = $carousel.current.scrollLeft
-
-    const items = getItemPositions()
-    // const target = closest(direction, items, currentItem ? currentItem : currentScrollPosition)
-    const target = closest("next", items, currentScrollPosition)
-    console.log(target)
-    setNavigationInactive({
-      next: items.indexOf(target) === $items.current.length - 1,
-      prev: items.indexOf(target) === 0
-    })
-    // setCurrentItem(items.indexOf(target))
   }, [])
+}
 
-  const childrenWithRef = Children.map(children, (Child, index) => {
-    return <Child.type { ...Child.props } key={index} ref={ el => $items.current[index] = el  }/>
-  })
+export default function Carousel({ children, className, size }){
+  useSmoothScroll()
 
-  const getItemPositions = () => {
-    if ($items.current.length < 1) { return [] }
+  const [navButtons, setNavButtons] = useNavButtons()
 
-    const firstItemOffset = $items.current[0].offsetLeft
-    return $items.current.map((item , i) => Math.round(item.offsetLeft - firstItemOffset))
-
+  const refs = {
+    carousel: useRef(null),
+    slides: useRef([]), // Array of refs for child nodes
   }
 
-  const updateNavigationState = (items, target) {
-    setNavigationInactive({
-      next: items.indexOf(target) === $items.current.length - 1,
-      prev: items.indexOf(target) === 0
-    })
-  }
+  const style = size ? { "--card-size" : size } : {} // Modify css variables here
 
-  const getNavigationTarget = (direction) => {
-    const currentScrollPosition = $carousel.current.scrollLeft
+  const scrollToSlide = (direction) => {
+    if (!refs.carousel.current) { return }
 
-    const items = getItemPositions()
-    const target = closest(direction, items, currentScrollPosition)
+    const getSlidePositions = () => {
+      // We check this with every interaction, as the exact positions can change when the screen is resized.
+      if (refs.slides.current.length < 1) { return [] }
 
-    updateNavigationState(items, target)
+      // We check the offset of the first slide to see if there is any padding, and remove it from the scroll value
+      const firstSlideOffset = refs.slides.current?.[0].offsetLeft || 0
+      return refs.slides.current.map(
+        (slide , i) => Math.round(slide.offsetLeft - firstSlideOffset)
+      )
+    }
 
-  }
+    const getNextSlide = (direction) => {
+      // Find the scroll position of the container
+      const currentScrollPosition = refs.carousel.current.scrollLeft
+      // Get the current left offset of all the slides (this can change when the screen is resized)
+      const slidePositions = getSlidePositions()
+      // Figure out which slide we should scroll to by looking for the closest slide to the current scroll poisiton
+      const closestSlide = closest(direction, slidePositions, currentScrollPosition)
 
-  const scrollToItem = (e) => {
-    if (!$items?.current || !$carousel.current) { return }
+      // Update the state to toggle enabled/disable of the navigation buttons
+      setNavButtons(slidePositions, closestSlide)
+      return closestSlide
+    }
 
-    const direction = e.currentTarget.getAttribute('data-direction')
-    getNavigationTarget(direction)
-
-
-    // setCurrentItem(target)
-    $carousel.current.scrollTo({
-      left: target,
+    // Use SmoothScroll polyfill to ensure Safari scrolls nicely
+    refs.carousel.current.scrollTo({
+      left: getNextSlide(direction),
       behavior: "smooth"
     })
-
-    // setTimeout(()=>{
-    //   setCurrentItem(null)
-    // }, 300)
-
   }
 
-  // const clearTarget = () => {
-  //   if (currentItem) setCurrentItem(null)
-  // }
+  // Add a ref to each slide so we can measure it
+  const slides = Children.map(children, (Child, index) =>
+    <Child.type
+      { ...Child.props }
+      key={index}
+      ref={ el => refs.slides.current[index] = el  }
+    />
+  )
 
-
-return <>
+  return <Fragment>
   <div
     className={ `${styles.carousel} ${className} focus:ring-4 focus:ring-blue-500` }
     tabIndex="-1"
-    ref={ $carousel }
+    ref={ refs.carousel }
     style={ style }
   >
     <div className={ styles.content }>
-      { childrenWithRef }
+      { slides }
     </div>
   </div>
     <div className="flex space-x-4 justify-center items-center h-12">
-      <ArrowButton direction="prev" onClick={ scrollToItem } disabled={ navigationInactive.prev }></ArrowButton>
-      <ArrowButton direction="next" onClick={ scrollToItem } disabled={ navigationInactive.next }></ArrowButton>
+      <NavButton
+        onClick={ () => { scrollToSlide("prev") } }
+        disabled={ navButtons.prev }
+      />
+      <NavButton
+        direction="next"
+        onClick={ () => { scrollToSlide("next") } }
+        disabled={ navButtons.next }
+      />
     </div>
-
-  </>
+  </Fragment>
 }
 
-const ArrowButton = ({ direction, onClick, className, disabled }) => {
+const NavButton = ({ direction, onClick, className, disabled }) => {
   const disabledClass = disabled ? "opacity-50" : ''
   const directionClass = direction === "next" ? 'transform scale-x-[-1]' : ''
 
   return <button
     onClick={ onClick }
-    data-direction={ direction }
     disabled={ disabled }
-    className="focus:ring-4 focus:ring-blue-500 p-1 rounded-full"
-    >
-      <Arrow
-        className={ `${directionClass} ${className} ${disabledClass}` }
-        width="24"
-        height="24"
-      ></Arrow>
+    className={ `focus:ring-4 focus:ring-blue-500 p-1 rounded-full ${className}` }
+  >
+    <Arrow
+      className={ `${directionClass} ${disabledClass}` }
+      width="24"
+      height="24"
+    />
   </button>
 }
