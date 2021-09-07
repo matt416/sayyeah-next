4a416d743f381d7302b555f7e453dd22b17122d1
Matt Rintoul <matt@sayyeah.com>
2021-09-07 10:47:25 -0400

    Add case studies, fix carousel

--- ui/stackitems/CaseStudies.js
+++ ui/stackitems/CaseStudies.js
@@ -1,8 +1,19 @@
 import { StackItem, StackContent } from "ui/components/Stack"
+import Card from "ui/components/Card"
+
+const CASE_STUDIES = {
+
+}
+
 
 export default function CaseStudies({}) {
   return <StackItem className="bg-black text-yellow min-h-[40rem]">
     <StackContent className="grid grid-cols-1 gap-12">
+
+      <Card>
+        My card is your card
+      </Card>
+
     </StackContent>
   </StackItem>
 }
