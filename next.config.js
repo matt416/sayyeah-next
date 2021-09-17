c58f6965f3da268a4873cc353e94b2e0365fb66c
Matt Rintoul <matt@sayyeah.com>
2021-09-17 11:02:23 -0400

    trying old proxy method

--- next.config.js
+++ next.config.js
@@ -22,4 +22,19 @@ module.exports = {
   //     ],
   //   }
   // }
+
+  async rewrites() {
+    return [
+      // we need to define a no-op rewrite to trigger checking
+      // all pages/static files before we attempt proxying
+      {
+        source: '/:path*',
+        destination: '/:path*'
+      },
+      {
+        source: '/:path*',
+        destination: `ttps://kanetix.sayyeah.work/:path*`
+      }
+    ]
+  }
 };
