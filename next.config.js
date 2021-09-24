a0f875fdecd91a90877fe3c7a7e8c7382f0c7122
Matt Rintoul <matt@sayyeah.com>
2021-09-24 13:44:01 -0400

    Another proxy url

--- next.config.js
+++ next.config.js
@@ -28,7 +28,7 @@ module.exports = {
       fallback: [
         {
           source: '/:path*',
-          destination: `https://live.sayyeah.work/:path*`,
+          destination: `https://live.sayyeah.work/:path*/`,
         },
       ],
     }
