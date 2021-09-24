b18b96e6b656b7cb8db6eb7c7c4fe837fd5a5ee9
Matt Rintoul <matt@sayyeah.com>
2021-09-24 13:07:52 -0400

    Update redirect fallback

--- next.config.js
+++ next.config.js
@@ -24,17 +24,13 @@ module.exports = {
   // }
 
   async rewrites() {
-    return [
-      // we need to define a no-op rewrite to trigger checking
-      // all pages/static files before we attempt proxying
-      {
-        source: '/:path*',
-        destination: '/:path*'
-      },
-      {
-        source: '/:path*',
-        destination: `http://localhost:3000/:path*`
-      }
-    ]
+    return {
+      fallback: [
+        {
+          source: '/:path*',
+          destination: `https://live.sayyeah.work/:path*`,
+        },
+      ],
+    }
   }
-};
+}
