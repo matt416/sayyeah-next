7cd66974f3b09c32d0caacae2127551513d85181
Matt Rintoul <matt@sayyeah.com>
2021-10-08 14:06:51 -0400

    Tailwind merge dependency

--- ui/components/Badge.js
+++ ui/components/Badge.js
@@ -1,15 +1,18 @@
 import { traits } from 'helpers/traits'
+import { twMerge } from 'tailwind-merge'
 
 const TRAIT = traits({
   default: "px-2.5 py-0.5 rounded-full",
   color: {
-    default: "bg-yeah-yellow text-black",
+    default: "px-2.5 py-0.5 rounded-full",
     black: "bg-black text-yeah-yellow"
   }
 })
 
-export default function Badge({ children, color, className }){
+export default function Badge({ children, color, className, ...props }){
 
-  return <span className={ TRAIT.apply({ color }, className) }>{ children }</span>
+  const appearance = twMerge("px-2.5 py-0.5 rounded-full bg-yeah-yellow text-black", className)
+// TRAIT.apply({ color }, className)
+  return <span className={ appearance }>{ children }</span>
 
 }
