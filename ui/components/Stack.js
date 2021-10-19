fd0627a977dbbedfe75428f6a9f17efdea3f5c91
Matt Rintoul <matt@sayyeah.com>
2021-10-19 11:26:16 -0400

    Prune deprecated components

--- ui/components/Stack.js
+++
@@ -1,7 +0,0 @@
-export { default as StackItem } from "./StackItem"
-export { default as StackContent } from "./StackContent"
-
-export const Stack = ({ children, id = "main-content" }) =>
-  <main tabIndex="-1" id={ id } className="flex flex-col items-center justify-center flex-1">
-  { children }
-  </main>
