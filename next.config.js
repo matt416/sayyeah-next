5a201bc09d6150fdd6c8231f2e42590874d70543
Matt Rintoul <matt@sayyeah.com>
2022-02-17 16:25:59 -0500

    Fix redirect search string bug

--- next.config.js
+++ next.config.js
@@ -23,7 +23,7 @@ module.exports = {
   //     ],
   //   }
   // }
-  trailingSlash:true,
+  trailingSlash: true,
 
   // async headers() {
   //   return [
@@ -38,93 +38,93 @@ module.exports = {
   //     },
   //   ]
   // },
-  async redirects(){
+  async redirects() {
     return [
       {
         source: '/essential',
         destination: '/essential-website-audit/',
         permanent: true,
-      },{
+      }, {
         source: '/essential-audit/',
         destination: '/essential-website-audit/',
         permanent: true
-      },{
+      }, {
         source: '/services/user-experience-design-service/',
         destination: '/services/user-experience-design/',
         permanent: true
-      },{
+      }, {
         source: '/services/user-interface-design-service/',
         destination: '/services/user-interface-design/',
         permanent: true
-      },{
+      }, {
         source: '/services/front-end-web-development-service/',
         destination: '/services/front-end-web-development/',
         permanent: true
-      },{
+      }, {
         source: '/digital-insights/user-experience-is-a-feeling-uxmatters/',
         destination: '/digital-insights/user-experience-is-a-feeling/',
         permanent: true
-      },{
+      }, {
         source: '/digital-insights/website-builder-accessibility-comparison/',
         destination: '/digital-insights/2021-website-builder-accessibility-comparison/',
         permanent: true
-      },{
+      }, {
         source: '/services/inclusive-website-design/',
         destination: '/solutions/inclusive-website-design/',
         permanent: true
-      },{
+      }, {
         source: '/solutions/organizational-wide-digital-innovation/',
         destination: '/solutions/',
         permanent: true
-      },{
+      }, {
         source: '/approach/inclusive-design/',
         destination: '/solutions/inclusive-design/',
         permanent: true
-      },{
+      }, {
         source: '/approach/service-design/',
         destination: '/solutions/service-design/',
         permanent: true
-      },{
+      }, {
         source: '/approach/product-strategy/',
         destination: '/solutions/product-strategy/',
         permanent: true
-      },{
+      }, {
         source: '/solutions/new-digital-initiatives/',
         destination: '/solutions/',
         permanent: true
-      },{
+      }, {
         source: '/solutions/existing-digital-initiatives/',
         destination: '/solutions/',
         permanent: true
-      },{
+      }, {
         source: '/approach/research/',
         destination: '/solutions/market-research/',
         permanent: true
-      },{
+      }, {
         source: '/social/',
         destination: '/contact-us/',
         permanent: true
-      },{
+      }, {
         source: '/essential-website-audit/terms-and-conditions/',
         destination: '/inclusive-website-audit/terms-and-conditions/',
         permanent: false,
-      },{
+      }, {
         source: '/essential-website-audit/starter/',
         destination: '/inclusive-website-audit/starter/',
         permanent: true,
-      },{
+      }, {
         source: '/essential-website-audit/custom/',
         destination: '/inclusive-website-audit/custom/',
         permanent: true,
-      },{
+      }, {
         source: '/essential-website-audit/comprehensive-website-audit-process/',
         destination: '/inclusive-website-audit/inclusive-website-audit-process/',
         permanent: true,
-      },{
-        source: '/essential-website-audit/checkout/?product=essential_audit_starter',
-        destination: '/inclusive-website-audit/checkout/?product=essential_audit_starter',
+      }, {
+        source: '/essential-website-audit/checkout/',
+        destination: '/inclusive-website-audit/checkout/',
         permanent: true,
-      },{
+      }, {
         source: '/essential-website-audit/request-custom-quote/',
         destination: '/inclusive-website-audit/request-custom-quote/',
         permanent: true,
