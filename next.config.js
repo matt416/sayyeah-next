a4a2b91688cdfe83bae87e7f5b246515607d39f0
Matt Rintoul <matt@sayyeah.com>
2023-01-16 13:12:19 -0500

    fix: redirects

--- next.config.js
+++ next.config.js
@@ -124,14 +124,14 @@ module.exports = {
         source: '/essential-website-audit/checkout/',
         destination: '/inclusive-website-audit/checkout/',
         permanent: true,
-      }, {
-        source: '/improve-learning/ ',
-        destination: '/digital-insights/improved-online-learning/',
-        permanent: true,
       },
       {
         source: '/essential-website-audit/request-custom-quote/',
         destination: '/inclusive-website-audit/request-custom-quote/',
+        permanent: true,
+      }, {
+        source: '/improve-learning/ ',
+        destination: '/digital-insights/improved-online-learning/',
         permanent: true,
       },
 
