9439368e8fa89e1ea93ac3f64a3ea81ac0039b33
Matt Rintoul <matt@sayyeah.com>
2023-09-20 13:52:40 -0400

    Add inclusive guide redirect

--- next.config.js
+++ next.config.js
@@ -41,15 +41,15 @@ module.exports = {
   async redirects() {
     return [
       {
-        source: '/',
+        source: "/",
         has: [
           {
-            type: 'host',
-            value: 'sayyeah.courses',
+            type: "host",
+            value: "sayyeah.courses",
           },
         ],
         permanent: false,
-        destination:"/solutions/elearning-services/",
+        destination: "/solutions/elearning-services/",
       },
       {
         source: "/case-study/george-brown-college",
@@ -57,111 +57,141 @@ module.exports = {
         permanent: true,
       },
       {
-        source: '/essential',
-        destination: '/essential-website-audit/',
-        permanent: true,
-      }, {
-        source: '/essential-audit/',
-        destination: '/essential-website-audit/',
-        permanent: true
-      }, {
-        source: '/services/user-experience-design-service/',
-        destination: '/services/user-experience-design/',
-        permanent: true
-      }, {
-        source: '/services/user-interface-design-service/',
-        destination: '/services/user-interface-design/',
-        permanent: true
-      }, {
-        source: '/services/front-end-web-development-service/',
-        destination: '/services/front-end-web-development/',
-        permanent: true
-      }, {
-        source: '/digital-insights/user-experience-is-a-feeling-uxmatters/',
-        destination: '/digital-insights/user-experience-is-a-feeling/',
-        permanent: true
-      }, {
-        source: '/digital-insights/website-builder-accessibility-comparison/',
-        destination: '/digital-insights/2021-website-builder-accessibility-comparison/',
-        permanent: true
-      }, {
-        source: '/services/inclusive-website-design/',
-        destination: '/solutions/inclusive-website-design/',
-        permanent: true
-      }, {
-        source: '/solutions/organizational-wide-digital-innovation/',
-        destination: '/solutions/',
-        permanent: true
-      }, {
-        source: '/approach/inclusive-design/',
-        destination: '/solutions/inclusive-design/',
-        permanent: true
-      }, {
-        source: '/approach/service-design/',
-        destination: '/solutions/service-design/',
-        permanent: true
-      }, {
-        source: '/approach/product-strategy/',
-        destination: '/solutions/product-strategy/',
-        permanent: true
-      }, {
-        source: '/solutions/new-digital-initiatives/',
-        destination: '/solutions/',
-        permanent: true
-      }, {
-        source: '/solutions/existing-digital-initiatives/',
-        destination: '/solutions/',
-        permanent: true
-      }, {
-        source: '/approach/research/',
-        destination: '/solutions/market-research/',
-        permanent: true
-      }, {
-        source: '/social/',
-        destination: '/contact-us/',
-        permanent: true
-      }, {
-        source: '/essential-website-audit/terms-and-conditions/',
-        destination: '/inclusive-website-audit/terms-and-conditions/',
-        permanent: false,
-      }, {
-        source: '/essential-website-audit/starter/',
-        destination: '/inclusive-website-audit/starter/',
-        permanent: true,
-      }, {
-        source: '/essential-website-audit/custom/',
-        destination: '/inclusive-website-audit/custom/',
-        permanent: true,
-      }, {
-        source: '/essential-website-audit/comprehensive-website-audit-process/',
-        destination: '/inclusive-website-audit/inclusive-website-audit-process/',
-        permanent: true,
-      }, {
-        source: '/essential-website-audit/checkout/',
-        destination: '/inclusive-website-audit/checkout/',
+        source: "/essential",
+        destination: "/essential-website-audit/",
         permanent: true,
       },
       {
-        source: '/essential-website-audit/request-custom-quote/',
-        destination: '/inclusive-website-audit/request-custom-quote/',
-        permanent: true,
-      }, {
-        source: '/improve-learning/ ',
-        destination: '/digital-insights/improved-online-learning/',
+        source: "/essential-audit/",
+        destination: "/essential-website-audit/",
         permanent: true,
       },
-
-    ]
+      {
+        source: "/services/user-experience-design-service/",
+        destination: "/services/user-experience-design/",
+        permanent: true,
+      },
+      {
+        source: "/services/user-interface-design-service/",
+        destination: "/services/user-interface-design/",
+        permanent: true,
+      },
+      {
+        source: "/services/front-end-web-development-service/",
+        destination: "/services/front-end-web-development/",
+        permanent: true,
+      },
+      {
+        source: "/digital-insights/user-experience-is-a-feeling-uxmatters/",
+        destination: "/digital-insights/user-experience-is-a-feeling/",
+        permanent: true,
+      },
+      {
+        source: "/digital-insights/website-builder-accessibility-comparison/",
+        destination:
+          "/digital-insights/2021-website-builder-accessibility-comparison/",
+        permanent: true,
+      },
+      {
+        source: "/services/inclusive-website-design/",
+        destination: "/solutions/inclusive-website-design/",
+        permanent: true,
+      },
+      {
+        source: "/solutions/organizational-wide-digital-innovation/",
+        destination: "/solutions/",
+        permanent: true,
+      },
+      {
+        source: "/approach/inclusive-design/",
+        destination: "/solutions/inclusive-design/",
+        permanent: true,
+      },
+      {
+        source: "/approach/service-design/",
+        destination: "/solutions/service-design/",
+        permanent: true,
+      },
+      {
+        source: "/approach/product-strategy/",
+        destination: "/solutions/product-strategy/",
+        permanent: true,
+      },
+      {
+        source: "/solutions/new-digital-initiatives/",
+        destination: "/solutions/",
+        permanent: true,
+      },
+      {
+        source: "/solutions/existing-digital-initiatives/",
+        destination: "/solutions/",
+        permanent: true,
+      },
+      {
+        source: "/approach/research/",
+        destination: "/solutions/market-research/",
+        permanent: true,
+      },
+      {
+        source: "/social/",
+        destination: "/contact-us/",
+        permanent: true,
+      },
+      {
+        source: "/essential-website-audit/terms-and-conditions/",
+        destination: "/inclusive-website-audit/terms-and-conditions/",
+        permanent: false,
+      },
+      {
+        source: "/essential-website-audit/starter/",
+        destination: "/inclusive-website-audit/starter/",
+        permanent: true,
+      },
+      {
+        source: "/essential-website-audit/custom/",
+        destination: "/inclusive-website-audit/custom/",
+        permanent: true,
+      },
+      {
+        source: "/essential-website-audit/comprehensive-website-audit-process/",
+        destination:
+          "/inclusive-website-audit/inclusive-website-audit-process/",
+        permanent: true,
+      },
+      {
+        source: "/essential-website-audit/checkout/",
+        destination: "/inclusive-website-audit/checkout/",
+        permanent: true,
+      },
+      {
+        source: "/essential-website-audit/request-custom-quote/",
+        destination: "/inclusive-website-audit/request-custom-quote/",
+        permanent: true,
+      },
+      {
+        source: "/improve-learning/ ",
+        destination: "/digital-insights/improved-online-learning/",
+        permanent: true,
+      },
+      {
+        source: "/inclusiveguide",
+        destination: "https://inclusive-design.sayyeah.courses",
+        permanent: true,
+      },
+    ];
   },
 
-  async rewrites() { // Fallback on production, don't do this locally please!
-    return process.env.NODE_ENV === "production" ? {
-      fallback: [
-        {
-          source: '/:path*',
-          destination: `https://insights.sayyeah.com/:path*/`,
-        },
-      ],
-    } : []
-  }
-}
+  async rewrites() {
+    // Fallback on production, don't do this locally please!
+    return process.env.NODE_ENV === "production"
+      ? {
+          fallback: [
+            {
+              source: "/:path*",
+              destination: `https://insights.sayyeah.com/:path*/`,
+            },
+          ],
+        }
+      : [];
+  },
+};
